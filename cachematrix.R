d185b08bc2defed75be83e805087c920d68dd1eb
emshilova <75572574+emshilova@users.noreply.github.com>
2020-12-06 22:05:40 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## For this assignment we are tring to optimize working on 
+## big amounts of data that need to be analyzed repeatedly.
 
-## Write a short comment describing this function
+## This first function gets the input 
+## and can create an inverse matrix and cache it.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+	set <- function(y){
+		x <<- y
+		m <<- NULL
+	}
+	get <- function()x
+	setInverse <- function(inverse) m <<- inverse
+	getInverse <- function() m 
+	list(set = set, get = get, 
+		setInverse = setInverse, 
+		getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## This second function is meant to return the value 
+## for the inverted matrix, but it can first check 
+## if the values are cached and use the existing version for output.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	m <- x$getInverse()
+	if(!is.null(m)){
+		message("getting cached data")
+		return(m)
+	}
+	data <- x$get()
+	m <- solve(data,...)
+	x$setInverse(m)
+	m
 }
+
+##Now we might check if the functions work.
+
+m <- matrix(rnorm(4),2,2)
+m1 <- makeCacheMatrix(m)
